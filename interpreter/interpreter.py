4817414650f5e9280155477580b0153fbd6b394e
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-06 11:46:37 +0000

    Tue, May 6, 2025, 7:46 AM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,2 +1,14 @@
 operation = input("Expression: ").strip()
 
+x,y,z = operation.split(" ")
+
+match y:
+    case "+":
+        print(x+z)
+    case "-":
+        print(x-z)
+    case"*":
+        print(x*z)
+    case"/":
+        print(x/z)
+
