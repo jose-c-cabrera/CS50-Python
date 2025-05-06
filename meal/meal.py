bdb5e5949077a02f7dc069497d5103cf918a24d1
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-06 12:11:36 +0000

    Tue, May 6, 2025, 8:11 AM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,12 @@
 def main():
     actualTime = input("What time is it? ")
+    convert(actualTime)
+    
 
 def convert(time):
     hours, minutes = time.split(":")
     minutes = float(minutes/60)
+    convertedToFloat = (float(hours+minutes))
+    return convertedToFloat
+
 
