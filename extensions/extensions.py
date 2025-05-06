b3768f911d30177514a6c36e29193e2a1fd85448
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-06 11:24:21 +0000

    Tue, May 6, 2025, 7:24 AM -04:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,18 @@
-filetype = input("File name:" ).split()
+filetype = input("File name:" ).strip().lower()
 
-match
+if filetype.endswith(".gif"):
+    print("image/gif")
+elif filetype.endwith(".jpg"):
+    print("image/jpg")
+elif filetype.endwith(".jpeg"):
+    print("image/jpeg")
+elif filetype.endwith(".png"):
+    print("image/png")
+elif filetype.endwith(".pdf"):
+    print("application/pdf")
+elif filetype.endwith(".txt"):
+    print("text/plain")
+elif filetype.endswith(".zip")
+    print("application/zip")
+else:
+    print("application/octet-stream")
