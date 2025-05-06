b50f366a6168cf39110344794b667dc934af6151
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-06 11:07:41 +0000

    Tue, May 6, 2025, 7:07 AM -04:00

---
+++ deep/deep.py
@@ -0,0 +1,8 @@
+userinput = input("What is the Answer to the Great Question of Life, the Universe and Everything?").strip().lower()
+
+match userinput:
+    case "42" | "forty-two" | "forty two":
+        print ("Yes")
+    case _:
+        print ("No")
+
