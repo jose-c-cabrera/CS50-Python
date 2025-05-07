72094e040c5d5ae7765d77f9a5c0ab3cc33fb5f2
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-07 13:17:04 +0000

    Wed, May 7, 2025, 9:17 AM -04:00

---
+++ plates/plates.py
@@ -0,0 +1,15 @@
+#Must start with two letters
+#Max 6 characters - Min 2 characters
+#Numbers at the end
+#First number cannot be a 0
+#No periods, spaces, punctuaion marks allowed
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(proposition)
+    
