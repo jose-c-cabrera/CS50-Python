b311423934a598fa294be7a5d2c6c68063577f5d
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-07 12:47:59 +0000

    Wed, May 7, 2025, 8:47 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,11 +4,10 @@ while amount_due > 0:
     print(f"Amount due {amount_due}")
     money_input = int(input("Insert Coin: "))
 
-    if money_input == 25:
+    if money_input == 25 or money_input == 10 or money_input == 5:
         amount_due -= money_input
-    elif money_input == 10:
-        amount_due -= money_input
-    elif money_input == 5:
-        amount_due -= money_input
+
+print (f"Change Owed): {abs(amount_due)}")
+
 
 
