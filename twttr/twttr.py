9847ce69189ea3e7efe25aed8c5c373d7ba1dadf
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-07 13:10:39 +0000

    Wed, May 7, 2025, 9:10 AM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -6,7 +6,7 @@ def remove_vowels(string):
     twitter_word = ""
 
     for char in string:
-        if char == 'a' or char =='e' or char== 'i' or char == 'o' or char == 'u':
+        if char == 'a' or char =='e' or char== 'i' or char == 'o' or char == 'u'or char == 'A' or char =='E' or char== 'I' or char == 'O' or char == 'U':
             continue
         else:
             twitter_word+=char
