048efcf4be7d892d86fc374ebf4e5c0024b9794a
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-07 12:59:00 +0000

    Wed, May 7, 2025, 8:59 AM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,10 @@
-string = input("Input: ")
+def main ():
+    user_input = input("Input: ")
+    remove_vowels(user_input)
 
 def remove_vowels(string):
     for char in string:
         if char == 'a' or char =='e' or char== 'i' or char == 'o' or char == 'u':
-            user_input -= char
-            print user_input
+            string -= char
+            print (string)
+main()
