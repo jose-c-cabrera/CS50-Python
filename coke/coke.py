23033454de599c10d3f3b9cbc7e82cd689683bfc
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-07 12:36:05 +0000

    Wed, May 7, 2025, 8:36 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,4 +1,7 @@
-amount_due = 50
-money_input = input("Insert Coin: ")
-while amount_due != :
+while amount_due != 0:
+    amount_due = 50
+    money_input = input("Insert Coin: ")
+    if money_input = 25 or 10 or 5
+
+
 
