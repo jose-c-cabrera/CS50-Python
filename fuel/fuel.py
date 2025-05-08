106ff12f8b1af4a36894e37e261a8343d6df9ffd
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-08 13:38:26 +0000

    Thu, May 8, 2025, 9:38 AM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,13 +1,14 @@
 while True:
     try:
-        input = input("Fraction: ")
-        x,y = input.split(sep='/')
+        users_input = input("Fraction: ")
+        x,y = users_input.split(sep='/')
         fraction = round((int(x)/int(y))*100)
     except (ValueError, ZeroDivisionError):
         pass
     else:
         break
 percentage = f"{fraction}%"
+if 
 print(percentage)
 
 
