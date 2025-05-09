ced14568f0467d69a3fa0ed906286670b29256cc
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-09 14:32:25 +0000

    Fri, May 9, 2025, 10:32 AM -04:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -14,5 +14,44 @@ dates = [
 ]
 
 date = input("Date: ").strip()
-day,month,year = 
+day,month,year = date.rsplit(sep= " ")
+
+match month:
+    case "January":
+        print("01")
+    case "February":
+        print("02")
+    case "March":
+        print("03")
+    case "April":
+        print("04")
+    case "May":
+        print("05")
+    case "June":
+        print("06")
+    case "July":
+        print("07")
+    case "August":
+        print("08")
+    case "September":
+        print("09")
+    case "October":
+        print("10")
+    case "November":
+        print("11")
+    case "December":
+        print("12")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
