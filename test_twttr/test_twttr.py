1166a3eb0c776162303230b3cbc6a1a118b23f8b
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-12 19:38:15 +0000

    Mon, May 12, 2025, 3:38 PM -04:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -2,8 +2,23 @@ import sys
 
 from twttr import shorten
 
-def test_hello():
-    shorten("Hello")
+def test_all_vowels():
+    assert shorten("aeiouAEIOU") == ""
+
+def test_no_vowels():
+    assert shorten("rhythm") == "rhythm"
+
+def test_mixed_case():
+    assert shorten("TwItTeR") == "TwtTR"
+
+def test_sentence():
+    assert shorten("This is CS50") == "Ths s CS50"
+
+def test_numbers_and_symbols():
+    assert shorten("email@domain.com") == "ml@dmn.cm"
+
+def test_empty_string():
+    assert shorten("") == ""
 
 #print(shorten(sys.argv[1]))
 
