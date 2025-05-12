b8da3382c3908f6ae8bf75783c6dc165e14242a9
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-12 19:59:07 +0000

    Mon, May 12, 2025, 3:59 PM -04:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -6,7 +6,11 @@ def value(greeting):
 
     if greeting.startswith("Hello"):
         return(int(0))
+    elif greeting.startswith("hello"):
+        return(int(0))
     elif greeting.startswith("H"):
+        return(int(20))
+    elif greeting.startswith("h"):
         return(int(20))
     else:
         return(int(100))
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,5 +1,18 @@
 from bank import value
 
 
-def test_hello ():
+def test_Hi ():
     assert value("Hi") == int(20)
+
+def test_hola ():
+    assert value("hola") == int(20)
+
+def test_my_friend ():
+    assert value("my friend") == int(100)
+
+def test_Hello ():
+    assert value("Hello") == int(0)
+
+def test_hello ():
+    assert value("hello") == int(0)
+
