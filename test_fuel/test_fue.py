41d472c4592b25cef0e26514df52d69f17c064d9
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-13 13:20:05 +0000

    Tue, May 13, 2025, 9:20 AM -04:00

--- test_fuel/test_fue.py
+++ test_fuel/test_fue.py
@@ -1,3 +1,21 @@
 from refueling import convert, gauge
 
 
+def test_100():
+    assert convert()) == 100
+    assert gauge(int(100) == "F"
+
+# def test_no_vowels():
+#     assert shorten("rhythm") == "rhythm"
+
+# def test_mixed_case():
+#     assert shorten("TwItTeR") == "TwtTR"
+
+# def test_sentence():
+#     assert shorten("This is CS50") == "Ths s CS50"
+
+# def test_numbers_and_symbols():
+#     assert shorten("email@domain.com") == "ml@dmn.cm"
+
+# def test_empty_string():
+#     assert shorten("") == ""
