f2d2c2ee1a8ce0c54c96833c10cdfe81bab22742
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-13 12:31:11 +0000

    Tue, May 13, 2025, 8:31 AM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,22 +2,16 @@
 from plates import is_valid
 
 def test_hello():
-    assert is_valid("hello") == True
+    assert is_valid("AA") == True
 
 def test_CS50():
-    assert is_valid("CS50") == True
+    assert is_valid("A2") == False
 
 def test_numbers_first():
-    assert is_valid("50CS") == False
+    assert is_valid("2A") == False
 
 def test_zero_first():
-    assert is_valid("H0505") == False
+    assert is_valid("22") == False
 
 def test_name():
-    assert is_valid("Jose") == True
-
-def test_normal_plate():
-    assert is_valid("QC1403") == True
-
-def test0():
-    assert is_valid("") == False
+    assert is_valid(" 2") == False
