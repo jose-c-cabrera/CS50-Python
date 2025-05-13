d07a9465f7be42256a6d4bd908c61b4295ea15da
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-13 12:46:14 +0000

    Tue, May 13, 2025, 8:46 AM -04:00

---
+++ test_fuel/refueling.py
@@ -0,0 +1,26 @@
+
+def main():
+
+
+def convert(fraction):
+    
+while True:
+    try:
+        users_input = input("Fraction: ")
+        x,y = users_input.split(sep='/')
+        x = int(x)
+        y = int(y)
+        if x > y:
+            continue
+        percentage = round((x/y)*100)
+    except (ValueError, ZeroDivisionError):
+        pass
+    else:
+        break
+
+if percentage >= 99:
+    print("F")
+elif percentage <= 1:
+    print("E")
+else:
+    print(f"{percentage}%")
