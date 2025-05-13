bdd7dde7e5c19e73f6c139fadecfc8af445db362
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-13 12:11:19 +0000

    Tue, May 13, 2025, 8:11 AM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,17 +4,17 @@ from plates import is_valid
 def test_hello():
     assert is_valid("hello") == True
 
-def test_no_vowels():
+def test_CS50():
     assert is_valid("CS50") == True
 
-def test_mixed_case():
+def test_numbers_first():
     assert is_valid("50CS") == False
 
-def test_sentence():
+def test_zero_first():
     assert is_valid("H0505") == False
 
-def test_numbers_and_symbols():
+def test_name():
     assert is_valid("Jose") == True
 
-def test_empty_string():
+def test_normal_plate():
     assert is_valid("QC1403") == True
