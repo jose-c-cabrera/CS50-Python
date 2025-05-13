55c83a6b4da1a635707e4e40b6b4509300024f28
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-13 12:01:34 +0000

    Tue, May 13, 2025, 8:01 AM -04:00

---
+++ test_plates/plates.py
@@ -0,0 +1,29 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(proposition):
+
+    if len(proposition) < 2 or len(proposition) > 6:
+        return False
+
+    if not proposition[0:2].isalpha():
+        return False
+
+    if not proposition.isalnum():
+        return False
+
+    for i in range(len(proposition)):
+        if proposition[i].isdigit():
+            if proposition[i] == '0':
+                return False
+            if not proposition[i:].isdigit():
+                return False
+            break
+
+    return True
+
+main ()
