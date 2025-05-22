7f86acea37a9cb1073e5f3d27858a0d944649a6c
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-22 23:31:27 +0000

    Thu, May 22, 2025, 7:31 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,14 +1,27 @@
-import random
+import sys, random
 from pyfiglet import Figlet
-
 
 figlet = Figlet()
 
-figlet.getFonts()
+if len(sys.argv) < 2:
+    f = random.choice(figlet.getFonts())
+    figlet.setFont(font=f)
 
-figlet.setFont(font = random.)
+elif len(sys.argv) < 4 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    f = sys.argv[2]
+    figlet.setFont(font=f)
 
-input = input("Input:" )
-print(f"Output:  {figlet.renderText(input)}")
+else:
+    sys.exit("Invalid Usage")
+
+if f in figlet.getFonts():
+    input = input("Input:" )
+    print(f"Output:  {figlet.renderText(s)}")
+else:
+    sys.exit("Invalid Usage")
+
+
+
+
 
 
