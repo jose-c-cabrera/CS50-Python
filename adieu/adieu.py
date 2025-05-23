5c077c8549a459d999df00c70366dcedb36c79cb
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-23 00:01:00 +0000

    Thu, May 22, 2025, 8:01 PM -04:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -3,6 +3,12 @@ import inflect
 names = []
 
 while (True):
-    name_input = input("Name: ")
-    names.append(name_input)
+    try:
+        name_input = input("Name: ")
+        names.append(name_input)
+
+        for item in names:
+    except:
+        (ValueError, IndexError):
+        continue
 
