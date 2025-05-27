b7798ac1462a29b09c00e5054800e5923c7ae1f9
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-27 00:02:16 +0000

    Mon, May 26, 2025, 8:02 PM -04:00

--- game/game.py
+++ game/game.py
@@ -4,11 +4,26 @@ while True:
     try:
         level = int(input("Level: "))
         if level > 0:
-            random_number = random.randint(1, level)
             break
     except ValueError:
         pass
 
+secret = random.randint(1, level)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess < 1 or guess > level:
+            continue
+        if guess < secret:
+            print("Too small!")
+        elif guess > secret:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+    except ValueError:
+        pass
 
 
 
