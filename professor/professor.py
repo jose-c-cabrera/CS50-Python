8edc9f084bbf01e7653f84eb0d7cb9c04be672a9
jose-c-cabrera <joseccabrerama@gmail.com>
2025-05-27 14:21:59 +0000

    Tue, May 27, 2025, 10:21 AM -04:00

---
+++ professor/professor.py
@@ -0,0 +1,18 @@
+import random
+
+def main():
+
+
+
+def get_level():
+    try:
+        while True:
+            
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
