c6a2268d7b8ba424396f868f118f6c1a061e1dec
jose-c-cabrera <joseccabrerama@gmail.com>
2025-06-10 14:47:49 +0000

    Tue, Jun 10, 2025, 10:47 AM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,14 +1,18 @@
 import sys
 
+
 if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
+line_count = 0
+
 with open(sys.argv[1], "r") as file:
     for line in file:
-        line_count +=1
-        
+        if line.strip():
+            line_count +=1
+
 
 
 
