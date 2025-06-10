983947b33289445fae7a1a80b2235f2b1eb58e0e
jose-c-cabrera <joseccabrerama@gmail.com>
2025-06-10 15:14:09 +0000

    Tue, Jun 10, 2025, 11:14 AM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,10 +5,12 @@ if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
+else:
+    sys.exit("Not a python file")
 
 line_count = 0
 try:
-    with open(sys.argv[1], "r") as file:
+    with open(sys.argv[2], "r") as file:
         for line in file:
             if line.strip() and not line.startswith("#"):
                 line_count +=1
