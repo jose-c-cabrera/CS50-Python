56747374e264ab7d3020110216f1c192b65435ac
jose-c-cabrera <joseccabrerama@gmail.com>
2025-06-10 15:15:42 +0000

    Tue, Jun 10, 2025, 11:15 AM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
-else:
+elif not sys.argv[2].endswith(".py") :
     sys.exit("Not a python file")
 
 line_count = 0
