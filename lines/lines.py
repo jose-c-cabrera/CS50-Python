a778b82aa02d2499715cc44bc1fb9274f78419a0
jose-c-cabrera <joseccabrerama@gmail.com>
2025-06-10 15:10:24 +0000

    Tue, Jun 10, 2025, 11:10 AM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,12 +7,17 @@ elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
 line_count = 0
+try:
+    with open(sys.argv[1], "r") as file:
+        for line in file:
+            if line.strip() and not line.startswith("#"):
+                line_count +=1
+except FileNotFoundError:
+    sys.exit("File does not exist")
 
-with open(sys.argv[1], "r") as file:
-    for line in file:
-        if line.strip():
-            line_count +=1
-        
+print(line_count)
+
+
 
 
 
