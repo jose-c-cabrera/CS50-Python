4ca1678d0ba94330551a3af268b5498d088e3488
jose-c-cabrera <joseccabrerama@gmail.com>
2025-06-16 15:41:18 +0000

    Mon, Jun 16, 2025, 11:41 AM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,3 +1,10 @@
 import sys
 import PIL
 import
+
+if len(sys.argv) < 4:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 4:
+    sys.exit("Too many command-line arguments")
+elif not sys.argv[1].endswith(".csv"):
+    sys.exit("Not a csv file")
